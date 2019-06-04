ec58e2af7ba0ca152658269e914b691e754c76e8
Xiaoxuan Wang <xiaoxuanwang@Xiaoxuans-MacBook-Pro.local>
2019-06-04 11:53:04 -0700

    Add Client app

--- index.js
+++ index.js
@@ -27,5 +27,15 @@ app.use("/", index);
 app.use("/api", apiRoutes);
 app.use("/", authRoutes);
 
+if (process.env.NODE_ENV === 'production'){
+  //Express will serve up production assets like main.js main.css
+  app.use(express.static('client/build'));
+  //Express will serve up index.html if it doesnt recognize the route
+  const path = require('path');
+  app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
